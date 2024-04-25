bba530f4b650820c5cdc82671013c86967ddc0dd
JeanGabrielSilva <jeangabrielslima@gmail.com>
2024-04-25 20:51:00 -0300

    Adicionando rota login

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const routes = require ('./routes');
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Funcionou a rota?');
 });
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
-})
+});
